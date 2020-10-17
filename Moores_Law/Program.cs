00e277f83be35dd632422ba65325597d5a4816c6
Virgil-Florin Ciobanu <ciobanu.virgilflorin@student.uoradea.ro>
2020-10-17 15:53:29 +0300

    Terminat_program_Moores_law

--- Moores_Law/Program.cs
+++ Moores_Law/Program.cs
@@ -10,6 +10,19 @@ namespace Moores_law
     {
         static void Main(string[] args)
         {
+            int n, ani, luni;
+
+            Console.WriteLine("Introdiceti n: ");
+            
+            n = int.Parse(Console.ReadLine());
+
+            double raspuns;
+
+            raspuns = (Math.Log(n , 2)) * 18;
+
+            Console.WriteLine("Dupa " + (((int)raspuns)/12) + " ani si " + (((int)raspuns) % 12) + " luni puterea de calcul se va imbunatati de " + n + " ori");
+
+            Console.ReadKey();
         }
     }
 }
