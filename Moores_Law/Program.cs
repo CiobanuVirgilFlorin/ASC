2287f61d12a4d50e11a66682061bad0817ba3cf6
Virgil-Florin Ciobanu <ciobanu.virgilflorin@student.uoradea.ro>
2020-10-17 15:17:24 +0300

    Moores_law_assignment

---
+++ Moores_Law/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moores_law
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
